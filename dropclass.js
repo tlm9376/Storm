f1a723c15ed8501d8934c2fee00f9ecde87c2e4a
tlm9376 <90855941+tlm9376@users.noreply.github.com>
2021-12-15 12:26:01 -0500

    Update dropclass.js

--- dropclass.js
+++ dropclass.js
@@ -1,38 +1,31 @@
 class Drop {
-    constructor() {
-        this.x = random(-width, width);
-        this.y = random(-height, height);
-        this.z = random(width);      
-        this.pz = this.z;
-    }
-    update() {
-      this.z = this.z + speed;
-      if (this.z < 1) {
-          this.z = width;
-        this.x = random(-width, width);
-        this.y = random(-height, height);
-        this.pz = this.z;
-      }
-    }
+  constructor() {
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.z = random(width);      
+      this.zz = this.z;
+  }
   
-    show() {
-   
+  drip() {
+ 
+  //create the streaking line to seem drop like
 
-      fill(30,9);
-      
-      noStroke();
-      
-      var sx = map(this.x/this.z, 0, 1, 0, width);
-      var sy = map(this.y/this.z, 0, 1, 0, height);
-      for (var r = map(this.z, 0, width,12,0); r>0; r-=30){
-      
-      ellipse(sx, sy, r,r);    }
-      
-      var px = map(this.x/this.pz, 0, 1, 0, width);
-      var py = map(this.y/this.pz, 0, 1, 0, height);
-      this.pz = this.z;
-      
-      stroke(255);
-      line(px, py, sx, sy);
+    let cirx = map(this.x/this.z, 0, 1, 0, width);
+    let ciry = map(this.y/this.z, 0, 1, 0, height);
+  let linx = map(this.x/this.zz, 0, 1, 0, width);
+  let liny = map(this.y/this.zz, 0, 1, 0, height);
+    this.zz = this.z;
+    
+    stroke(255);
+    // strokeWeight(1)
+    line(linx, liny, cirx, ciry);
+  }
+fall() {
+    this.z = this.z + speed;
+    if (this.z < 1) {
+        this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.zz = this.z;
     }
-}
+  }}
